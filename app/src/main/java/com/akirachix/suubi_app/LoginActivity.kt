c8b0569acb30889166e366c551c38d0a8edccbb6
kimura-khamali <kimurakhamali@gmail.com>
2024-07-14 11:54:47 +0300

    added

---
+++ app/src/main/java/com/akirachix/suubi_app/LoginActivity.kt
@@ -0,0 +1,56 @@
+package com.akirachix.suubi_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.akirachix.suubi_app.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setContentView(binding.root)
+
+        binding.signUpTextView.setOnClickListener {
+            val intent = Intent(this,SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.checkLogin.setOnClickListener {
+            validateRegistration()
+        }
+
+    }
+
+    private fun validateRegistration() {
+        var formErr = false
+        clearErrors()
+
+        val monthlyDeduction = binding.sacco.text.toString()
+        if (monthlyDeduction.isBlank()) {
+            formErr = true
+            binding.sacco.error = "Monthly Deduction is required"
+        }
+
+        val password = binding.etconPassword.text.toString()
+        if (password.isBlank()) {
+            formErr = true
+            binding.etconPassword.error = "Password is required"
+        }
+
+//        if (!formErr) {
+//            // Proceed with registration or login
+//            val intent = Intent(this, loans_section_Activity::class.java)
+//            startActivity(intent)
+//        }
+    }
+
+    private fun clearErrors() {
+        binding.sacco.error = null
+        binding.etconPassword.error = null
+    }
+}
+
